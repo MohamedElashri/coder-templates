c37da1496e04b139c69c309a788b4ad87c5fc5e1
MohamedElashri <mail@elashri.com>
2024-03-03 08:10:20 +0200

    remove git config stuff

--- vscode-server-web/main.tf
+++ vscode-server-web/main.tf
@@ -37,19 +37,6 @@ resource "coder_agent" "main" {
     code-server --accept-server-license-terms serve-local --without-connection-token --quality stable --telemetry-level off >/tmp/code-server.log 2>&1 &
   EOT
 
-  # These environment variables allow you to make Git commits right away after creating a
-  # workspace. Note that they take precedence over configuration defined in ~/.gitconfig!
-  # You can remove this block if you'd prefer to configure Git manually or using
-  # dotfiles. (see docs/dotfiles.md)
-  env = {
-    GIT_AUTHOR_NAME     = "${data.coder_workspace.me.owner}"
-    GIT_COMMITTER_NAME  = "${data.coder_workspace.me.owner}"
-    GIT_AUTHOR_EMAIL    = "${data.coder_workspace.me.owner_email}"
-    GIT_COMMITTER_EMAIL = "${data.coder_workspace.me.owner_email}"
-  }
-}
-
-
 
 resource "coder_app" "code-server" {
   count        = 1
